cb0a09d1b7d010ac6c532898a151f18e40a379f7
dtriska <dtriska21@yahoo.com>
2024-04-27 22:57:41 -0700

    Client Comments

--- chat_client.py
+++ chat_client.py
@@ -5,20 +5,23 @@ import sys
 import threading
 
 HEADER_LENGTH = 10
-IP = "127.0.0.1"
-PORT = 8888
+IP = "127.0.0.1" # 127 is local address, should change if running on two different machines
+PORT = 8888 
 
+# set username, will be sent to server
 my_username = input("Enter your name: ")
 
+# Socket Creation
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
+client_socket.connect((IP, PORT)) # connect to server address
+client_socket.setblocking(False) # non-blocking mode
 
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 
 def receive_messages():
+    # continuosly receive messages from the server
     while True:
         try:
             while True:
@@ -33,21 +36,25 @@ def receive_messages():
                 message = client_socket.recv(message_length).decode('utf-8')
                 print(f'{username} > {message}')
 
+        # I/O error handling
         except IOError as e:
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
                 print('Reading error: {}'.format(str(e)))
                 sys.exit()
             continue
-
+        # other exceptions (just good practice)
         except Exception as e:
             print('Reading error: '.format(str(e)))
             sys.exit()
 
-receive_thread = threading.Thread(target=receive_messages)
-receive_thread.daemon = True
+# Receive thread is used to handle message from server continusosly
+# We dont block main thread from sending messages
+receive_thread = threading.Thread(target=receive_messages) 
+receive_thread.daemon = True # daemon makes this thread terminate when the main thread terminates
 receive_thread.start()
 
 while True:
+    # main thread for sending messages
     message = input(f'{my_username} > ')
 
     if message:
